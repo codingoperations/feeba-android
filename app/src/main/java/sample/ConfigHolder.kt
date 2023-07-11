f7c878590b2d2e01245f6d771868c95384de6a5d
iamzafar <hackistanuz@gmail.com>
2023-07-10 20:24:24 -0700

    config change for testing

--- app/src/main/java/sample/ConfigHolder.kt
+++ app/src/main/java/sample/ConfigHolder.kt
@@ -4,10 +4,15 @@ private val prodConfig = ServerConfig(
     "https://api.feeba.io",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjE4MDE5OTYsInBheWxvYWQiOnsidXNlcklkIjoiNjNkYzliMDFjN2IxNjYyZWE3MmQ3OTllIiwicHJvamVjdE5hbWUiOiJmZWViYSJ9LCJpYXQiOjE2NzU0MDE5OTZ9.VuIAQ90oQar5958nZHNKc6nOa8m3abzoFvGnk0cbpnY"
 )
+
 private val devConfig = ServerConfig(
     "https://dev-api.feeba.io",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjIxMTQ4NDQsInBheWxvYWQiOnsidXNlcklkIjoiNjNlMGIzNDQ1MzdkNzg2YWY0Yzc4OGMyIiwicHJvamVjdE5hbWUiOiJmZWViYSJ9LCJpYXQiOjE2NzU3MTQ4NDR9.crhC6URHc0NH5D3x-7GKgHLV8CNFeP4wpnzjbiiAgzI"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTUzMjA0NzYsInBheWxvYWQiOnsidXNlcklkIjoiNjM3OWEzY2ZiODMyZDE3ZmI2YmE1NTI4IiwicHJvamVjdE5hbWUiOiJBYmMifSwiaWF0IjoxNjY4OTIwNDc2fQ.RkiBEWqXTn9ozSIKDEK3PiUQP5SHwRGjJYnVErkyZdk" // for tests
 )
+//private val devConfig = ServerConfig(
+//    "https://dev-api.feeba.io",
+//    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjIxMTQ4NDQsInBheWxvYWQiOnsidXNlcklkIjoiNjNlMGIzNDQ1MzdkNzg2YWY0Yzc4OGMyIiwicHJvamVjdE5hbWUiOiJmZWViYSJ9LCJpYXQiOjE2NzU3MTQ4NDR9.crhC6URHc0NH5D3x-7GKgHLV8CNFeP4wpnzjbiiAgzI"
+//)
 
 object ConfigHolder {
     private var currentConfig = prodConfig
