122ce8cd955c68da0ee64146f3efff8337162215
Feeba <admin@feeba.io>
2023-09-13 15:23:04 -0700

    - Client token update

--- app/src/main/java/sample/ConfigHolder.kt
+++ app/src/main/java/sample/ConfigHolder.kt
@@ -2,12 +2,12 @@ package sample
 
 private val prodConfig = ServerConfig(
     "https://api.feeba.io",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjE4MDE5OTYsInBheWxvYWQiOnsidXNlcklkIjoiNjNkYzliMDFjN2IxNjYyZWE3MmQ3OTllIiwicHJvamVjdE5hbWUiOiJmZWViYSJ9LCJpYXQiOjE2NzU0MDE5OTZ9.VuIAQ90oQar5958nZHNKc6nOa8m3abzoFvGnk0cbpnY"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjIxMzMyNjEsInBheWxvYWQiOnsidXNlcklkIjoiNjNlMTY1NmI1YzQ2NWM4Y2U1NGE1NjY2IiwicHJvamVjdE5hbWUiOiJmZWViYSJ9LCJpYXQiOjE2NzU3MzMyNjF9.u1QKFr15wJQcEgyVjaLd0aU5A-XZoOaFGqFnGOSIXhE"
 )
 
 private val devConfig = ServerConfig(
     "https://dev-api.feeba.io",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTUzMjA0NzYsInBheWxvYWQiOnsidXNlcklkIjoiNjM3OWEzY2ZiODMyZDE3ZmI2YmE1NTI4IiwicHJvamVjdE5hbWUiOiJBYmMifSwiaWF0IjoxNjY4OTIwNDc2fQ.RkiBEWqXTn9ozSIKDEK3PiUQP5SHwRGjJYnVErkyZdk" // for tests
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjIxMTQ4NDQsInBheWxvYWQiOnsidXNlcklkIjoiNjNlMGIzNDQ1MzdkNzg2YWY0Yzc4OGMyIiwicHJvamVjdE5hbWUiOiJmZWViYSJ9LCJpYXQiOjE2NzU3MTQ4NDR9.crhC6URHc0NH5D3x-7GKgHLV8CNFeP4wpnzjbiiAgzI" // for tests
 )
 
 object ConfigHolder {
